243529ff8e76940987cc832d677dbc2b3ee28d07
Dmitry Shloma <dmitry.shloma@mail.ru>
2018-06-05 17:33:28 +0600

    refactoring, optimize memory usage

--- base-unit/loghelper.h
+++ base-unit/loghelper.h
@@ -20,6 +20,8 @@ enum MSG_MODE {USUAL, NOTIFY, CRITICAL}; // msg, MSG, !!! MSG !!!
 //typedef int MSG_MODE; // comment this if used avr-gcc
 
 void str_to_upper(const char *in, char *out);
+
+// msg max length is 20
 void format_msg_to_msg_log(const char *msg, char *msg_log, MSG_MODE mode);
 
 void to_log(LOG_DST dst, const char *msg, MSG_MODE mode);
@@ -29,25 +31,26 @@ void str_to_upper(const char *in, char *out)
     while ((*out++ = toupper(*in++)));
 }
 
+// msg max length is 20
 void format_msg_to_msg_log(const char *msg, char *msg_log, MSG_MODE mode)
 {
-    // add text decorate
-    char msg_mode[70] = ""; // TODO: may be is allocated dynamically?
+    // add decorate to msg
+    char msg_mode[24] = ""; // TODO: may be is allocated dynamically?
     if (mode == USUAL) {
-        strcpy(msg_mode, msg);
+        strncpy(msg_mode, msg, sizeof(msg_mode));
     } else if (mode == NOTIFY) {
-        str_to_upper(msg, msg_mode);
+        str_to_upper(msg, msg_mode); // TODO: сделать ограничение кол-ву символов
     } else if (mode == CRITICAL) {
         str_to_upper(msg, msg_mode);
-        char buf_temp[70] = ""; // TODO: may be is allocated dynamically?
-        sprintf(buf_temp, "!!! %s !!!", msg_mode);
-        strcpy(msg_mode, buf_temp);
+        char buf_temp[24] = ""; // TODO: may be is allocated dynamically?
+        snprintf(buf_temp, sizeof(msg_mode), "! %s !", msg_mode);
+        strncpy(msg_mode, buf_temp, sizeof(msg_mode));
     }
 
     // add datetime
     struct tm t;
     rtctime(&t);
-    char buf_datetime[70] = "";
+    char buf_datetime[21] = "";
     strftime(buf_datetime, sizeof(buf_datetime), "%Y-%m-%d, %H:%M:%S", &t);
     sprintf(msg_log, "[%s] %s", buf_datetime, msg_mode);
 }
@@ -56,7 +59,7 @@ void to_log(LOG_DST dst, const char *msg, MSG_MODE mode)
 {
     // TODO: if strlen(msg) > 247 :)
 
-    char msg_log[70] = "";
+    char msg_log[47] = "";
     format_msg_to_msg_log(msg, msg_log, mode);
 
     // write to dst
@@ -67,18 +70,18 @@ void to_log(LOG_DST dst, const char *msg, MSG_MODE mode)
         char msg[70] = "";
         switch (error) {
         case SDCARD_NO_ERROR:
-            sprintf(msg, "append \"%s\" in file \"%s\"", msg_log, LOG_FILE);
+            sprintf(msg, "append is success", msg_log);
             to_log(TO_SERIAL, msg, USUAL);
             break;
         case SDCARD_NOT_PRESENT:
-            to_log(TO_SERIAL, "sd-card failed, or not present", NOTIFY);
+            to_log(TO_SERIAL, "sd-card failed", NOTIFY);
             break;
         case SDCARD_OPEN_FILE_ERROR:
-            sprintf(msg, "error open file %s", LOG_FILE);
+            sprintf(msg, "error open file");
             to_log(TO_SERIAL, msg, NOTIFY);
             break;
         default:
-            sprintf(msg, "sdcard, unexpected error %d", error);
+            sprintf(msg, "sdcard, error %d", error);
             to_log(TO_SERIAL, msg, NOTIFY);
         }
     }
